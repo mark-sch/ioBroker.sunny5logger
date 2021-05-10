e5a328ba05c11595c147fd15d422b26996936037
mark-sch <mark@schmalohr.de>
2021-05-10 22:30:09 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'sunny5-logger adapter settings': {
+		'en': 'Adapter settings for sunny5-logger',
+		'de': 'Adaptereinstellungen für sunny5-logger',
+		'ru': 'Настройки адаптера для sunny5-logger',
+		'pt': 'Configurações do adaptador para sunny5-logger',
+		'nl': 'Adapterinstellingen voor sunny5-logger',
+		'fr': "Paramètres d'adaptateur pour sunny5-logger",
+		'it': "Impostazioni dell'adattatore per sunny5-logger",
+		'es': 'Ajustes del adaptador para sunny5-logger',
+		'pl': 'Ustawienia adaptera dla sunny5-logger',
+		'zh-cn': 'sunny5-logger的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
